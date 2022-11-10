8f605ff2c7ad2c1daed45ce9c770ce8e7bd736e4
ozodbek-web <ozodbektursunboyev77@gmail.com>
2022-11-10 19:35:43 +0500

    header ended

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Footer from "./Components/Footer/Footer";
-// import Header from "./Components/Header/Header";
+import Header from "./Components/Header/Header";
 import Main from "./Components/Main/Main";
 
 const App = () => {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <Main />
       <Footer />
     </>
